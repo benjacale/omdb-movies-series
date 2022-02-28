ad57699ade988edc5eac3b30146c8cc01ccac95b
benjacale <benjacale5@gmail.com>
2022-02-28 20:27:16 -0300

    make it better

--- api/config/db.js
+++ api/config/db.js
@@ -1,8 +1,18 @@
 const Sequelize = require("sequelize");
-const db        = new Sequelize("omdb", null, null, {
-    host: "localhost",
+const db        = new Sequelize(
+    "druvmglsoscid",
+    "nvvxhpumbnsmke", 
+    "02ebf03907b62adec8a8f6f858b8adf6ba662074a5e218eb1e04e631b0c60eba", {
+    
+    host: "ec2-54-156-110-139.compute-1.amazonaws.com",
     dialect: "postgres",
-    logging: false
+    logging: false,
+    port: 5432,
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
 });
 
 module.exports = db;
